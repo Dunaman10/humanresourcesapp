dade3cff274ea3437a1be74a52f308617cd01660
Syarif Hidayatullah <113423511+Dunaman10@users.noreply.github.com>
2026-01-02 15:07:47 +0700

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Scripts -->
         // @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @vite(['public/build/assets/app-D68rlCir.css', 'public/build/assets/app-PZj53Sw7.js'])
+        @vite(['/public/build/assets/app-D68rlCir.css', '/public/build/assets/app-PZj53Sw7.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">`
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-900 dark:bg-gray-900">
